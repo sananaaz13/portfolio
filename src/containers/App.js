023bab6aae63dc155c16f4ded225752f028725eb
sananaaz13 <52095426+snaaz331@users.noreply.github.com>
2021-01-30 12:12:18 +0000

    navbar

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,26 @@
-import React, {Components} from 'react';
+import React, {Component} from 'react';
+
+
 import './App.css';
 
 
-//Scrollspy npm package//
-import Scrollspy from 'react-scrollspy'
-
+//Scrollspy npm package
+//import Scrollspy from 'react-scrollspy';//
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 //Boostrap//
-<link
-  rel="stylesheet"
-  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-  crossorigin="anonymous"
-/>
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class Sana extends Components{
-  render(){
-    return(
-      <div>
 
-        <div>
-        <section id="section-1">section 1</section>
-        <section id="section-2">section 2</section>
-        <section id="section-3">section 3</section>
-        </div>
-
-        <Scrollspy items={ ['section-1', 'section-2', 'section-3'] } currentClassName="is-current">
-        <li><a href="#section-1">section 1</a></li>
-        <li><a href="#section-2">section 2</a></li>
-        <li><a href="#section-3">section 3</a></li>
-        </Scrollspy>
-
-      </div>
+class App extends Component{
+render() {
+  return (
+    <Navbar>
+      <Navbar.Brand href="#home">SANA NAAZ</Navbar.Brand>
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Navbar>
     );
   }
 }
